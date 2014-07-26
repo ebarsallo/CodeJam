680573a5986cf2ef96ac4636a99dc09ab8257bac
ebarsallo <ebarsallo@gmail.com>
2014-07-26 02:19:01 -0400

    First Commit

---
+++ python/cc9.py
@@ -0,0 +1,41 @@
+# Chapter 9| Recursion and Dynamic Programming
+
+# -----------------------------------------------------------------------------------------
+# Interview Questions
+# -----------------------------------------------------------------------------------------
+
+# 9.1   A child is running up a staircase with n steps, and can hop either 1 step, 2 steps,
+#       or 3 steps at a time. Implement a method to count how many possible ways the
+#       child can run up the stairs.
+
+def count_step(n, M):
+    if n == 0:
+        return 1
+    elif n < -1:
+        return 0
+    elif M[n]:
+        return M[n]
+    else:
+        M[n] = count_step (n-3, M) + count_step (n-2, M) + count_step (n-1, M)
+        return M[n]
+
+n = 4
+M = []
+M = [0 for x in range(n+1)]
+print "steps: ", count_step(n, M)
+
+
+# 9.2   Imagine a robot sitting on the upper left corner of an X by Y grid. The robot can
+#       only move in two directions: right and down. How many possible paths are there
+#       for the robot to go from (0,0) to (X,Y)?
+#       FOLLOW UP
+#       Imagine certain spots are "off limits", such that the robot cannot step on them.
+#       Design an algorithm to find a path for the robot from the top left to the bottom
+#       right.
+
+# 9.3   A magic index in an index A[0..n-1] is defined to be an index such that A[i]
+#       = i. Given a sorted array of distinct integers, write a method to find a magic
+#       index, if one exists, in array A.
+#       FOLLOW UP
+#       What if the values are not distinct?
+
