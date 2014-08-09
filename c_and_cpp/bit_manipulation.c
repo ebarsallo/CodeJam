285d1de9a0007a715b1bc02860e473073ac2e1cc
Servio Palacios <servio@palacios.com>
2014-08-09 18:23:22 -0400

    Bit manipulation exercises

---
+++ c_and_cpp/bit_manipulation.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+void displayBits(unsigned);
+
+int main(){
+  unsigned num = 960;
+  printf("\n The result of left shifting\n");
+  displayBits(num);
+  printf("8 bit position using the left shift operator << is\n");
+  displayBits(num<<8);
+  
+  printf("The result of right shifting\n");
+  displayBits(num);
+  printf("8 bit position using the right shift operator >> is\n");
+  displayBits(num>>8);
+
+  return 0;
+}
+
+void displayBits(unsigned value){
+  unsigned c, displayMask = 1 << 15;
+  printf("%7u = ", value);
+  
+  for(c=1; c<=16; c++){
+    putchar(value & displayMask ? '1' : '0');
+    value <<= 1;
+    
+    if( c%8 == 0)
+      putchar(' ');
+  }//For
+  putchar('\n');
+}//displayBits
